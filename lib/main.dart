329cf1f1c37c464b45c0f630cade4b49c774216c
bmurcia <bnmurci4@gmail.com>
2024-10-24 22:30:03 -0600

    Added like button and its funcionality

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,16 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+  void toggleFavorite(){
+    if(favorites.contains(current)){
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 
@@ -41,6 +51,13 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -48,11 +65,26 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10,),
-            ElevatedButton(
-              onPressed: (){
-                appState.getNext();
-              },
-              child: Text("Next")
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: (){
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('like'),
+                ),
+                SizedBox(width: 10,),
+                ElevatedButton(
+                  onPressed: (){
+                    appState.getNext();
+                  },
+                  child: Text("Next")
+                ),
+                
+                
+              ],
             )
           ],
         ),
