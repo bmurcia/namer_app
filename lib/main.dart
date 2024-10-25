fd9953d63ed77e7c4e4450ceb94c294a2aee9fa3
bmurcia <bnmurci4@gmail.com>
2024-10-25 12:54:08 -0600

    add a new page

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
 
       case 1:
-        page = Placeholder();
+        page = FavoritePage();
         break;
 
       default:
@@ -187,6 +187,34 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+
+class FavoritePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>(); 
+
+    if (appState.favorites.isEmpty){
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ''${appState.favorites.length} favorites: '),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asCamelCase),
+          ),
+      ],
     );
   }
 }
